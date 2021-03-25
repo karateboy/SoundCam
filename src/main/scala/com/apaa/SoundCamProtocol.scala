ad6fdb519e4df01ac16fc6fa59578d80e9d5e211
Aragorn <karateboy.tw@gmail.com>
2021-03-25 18:15:55 +0800

    update

--- src/main/scala/com/apaa/SoundCamProtocol.scala
+++ src/main/scala/com/apaa/SoundCamProtocol.scala
@@ -12,9 +12,34 @@ import java.net.InetSocketAddress
 object SoundCamProtocol {
   trait Command
   final case object FindSoundCam extends  Command
-  final case class SoundCamParam(ip:String, port:Int) extends Command
+  case class RequestHeader(cmd:Byte, invokeID:Byte, reserved:Short, len:Int)
+  case class ResponseHeader(cmd:Byte, invokeID:Byte, reserved:Short, status:Int, len:Int)
+  case class DataHeader(cmd:Byte, invokeID:Byte, reserved:Short, len:Int)
 
+  final case class SoundCamParam(ip:String, port:Int) extends Command
   case class ConnectSoundCam(ip:String, port:Int) extends  Command
+
+  val ResetReq = 0x80
+  val ResetRes = 0x0
+  val IdentificationReq = 0x84
+  val IdentificationRes = 0x4
+  val PrepareStateReq = 0x86
+  val PrepareStateRes = 0x6
+  val FinishStateReq = 0x87
+  val FinishStateRes = 0x7
+  val StopProcedureReq = 0x89
+  val StopProcedureRes = 0x9
+  val StartProcedureReq = 0x8A
+  val ReadDataObjectReq = 0x92
+  val ReadDataObjectRes = 0x12
+  val WriteDataObjectReq = 0x93
+  val WriteDataObjectRes = 0x13
+  val DataMessage = 0x41
+
+  case class ConnectReq(protocolVersion:Int) extends Command
+  case class ConnectResp(protocolVersion:Int,
+                         deviceID:Int) extends Command
+  case object Reset extends Command
 
 
   def apply(): Behavior[Command] = Behaviors.setup { context =>
